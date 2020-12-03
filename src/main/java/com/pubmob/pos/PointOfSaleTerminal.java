0553230874a2cf41e94a860a8b9768d0e85195f3
J. B. Rainsberger <me@jbrains.ca>
2020-12-03 11:41:22 -0400

    We now have a bare-bones terminal that starts, greets the user, then stops.
    Fixed a gradle deprecation warning by upgrading how we choose the application entry point class name.

---
+++ src/main/java/com/pubmob/pos/PointOfSaleTerminal.java
@@ -0,0 +1,8 @@
+package com.pubmob.pos;
+
+public class PointOfSaleTerminal {
+    public static void main(String[] args) {
+        System.out.println("Point of Sale Terminal.");
+        System.out.println("Done.");
+    }
+}
