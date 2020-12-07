655b68dc72275380363c3eeb65fa493f02564afb
J. B. Rainsberger <me@jbrains.ca>
2020-12-07 13:57:33 -0400

    Extracted 'matches quit command' to its own method.

--- src/main/java/com/pubmob/pos/PointOfSaleTerminal.java
+++ src/main/java/com/pubmob/pos/PointOfSaleTerminal.java
@@ -9,24 +9,34 @@ public class PointOfSaleTerminal {
     public static void main(String[] args) {
         System.out.println("Point of Sale Terminal.");
 
-        new BufferedReader(new InputStreamReader(System.in))
-                .lines()
-                .takeWhile(line -> !"q".equals(line))
-                .forEach(PointOfSaleTerminal::handleBarcode);
+        new BufferedReader(new InputStreamReader(System.in)).lines()
+                .takeWhile(PointOfSaleTerminal::isNotQuitCommand)
+                .map(PointOfSaleTerminal::handleBarcode)
+                .forEach(System.out::println);
 
         System.out.println("Done.");
     }
 
-    private static void handleBarcode(String barcode) {
+    private static boolean isNotQuitCommand(String line) {
+        return !"q".equals(line);
+    }
+
+    private static String handleBarcode(final String barcode) {
         final HashMap<String, Integer> barcodeToPrice = HashMap.of("12345", 550, "23456", 1015);
 
-        System.out.println(
-                barcodeToPrice.get(barcode)
-                        .map(PointOfSaleTerminal::formatPrice)
-                        .getOrElse("Error: barcode not found."));
+        return barcodeToPrice.get(barcode)
+                .map(PointOfSaleTerminal::formatPrice)
+                .getOrElse("Error: barcode not found.");
     }
 
     private static String formatPrice(int cents) {
         return String.format("$%.2f", cents / 100d);
     }
 }
+
+
+
+
+
+
+
