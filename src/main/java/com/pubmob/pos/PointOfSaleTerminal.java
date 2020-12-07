7961784c643a5f694a9a7089d6b317b1aa90ffa1
J. B. Rainsberger <me@jbrains.ca>
2020-12-07 12:46:05 -0400

    We can handle one barcode if it's equal to 12345.

--- src/main/java/com/pubmob/pos/PointOfSaleTerminal.java
+++ src/main/java/com/pubmob/pos/PointOfSaleTerminal.java
@@ -1,8 +1,20 @@
 package com.pubmob.pos;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class PointOfSaleTerminal {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Point of Sale Terminal.");
+
+        final BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        final String barcode = input.readLine();
+        if ("12345".equals(barcode))
+            System.out.println("$5.50");
+        else
+            System.out.println("Error: barcode not found.");
+
         System.out.println("Done.");
     }
 }
