2002505c9e46801a43195bf45a6fac59300a76dc
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2020-12-07 17:59:14 +0100

    The command line interface now handles an infinite stream of input

--- src/main/java/com/pubmob/pos/PointOfSaleTerminal.java
+++ src/main/java/com/pubmob/pos/PointOfSaleTerminal.java
@@ -1,24 +1,26 @@
 package com.pubmob.pos;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class PointOfSaleTerminal {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         System.out.println("Point of Sale Terminal.");
 
         final BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        final String barcode = input.readLine();
-        if("q".equals(barcode)) {
+        input.lines().forEach(PointOfSaleTerminal::handleBarcode);
 
+        System.out.println("Done.");
+    }
+
+    private static void handleBarcode(String barcode) {
+        if ("q".equals(barcode)) {
+            System.exit(0);
         } else {
             if ("12345".equals(barcode))
                 System.out.println("$5.50");
             else
                 System.out.println("Error: barcode not found.");
         }
-
-        System.out.println("Done.");
     }
 }
