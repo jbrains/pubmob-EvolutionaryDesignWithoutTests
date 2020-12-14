58b180059519bdbe0effa37d45759cc310b251da
Osvaldas Grigas <og@platform-lunar.com>
2020-12-14 19:27:55 +0200

    mob next [ci-skip]

--- src/main/java/com/pubmob/pos/PointOfSaleTerminal.java
+++ src/main/java/com/pubmob/pos/PointOfSaleTerminal.java
@@ -16,10 +16,17 @@ public class PointOfSaleTerminal {
         new BufferedReader(new InputStreamReader(System.in)).lines()
                 .map(String::trim)
                 .takeWhile(PointOfSaleTerminal::isNotQuitCommand)
-                .map(barcodeInventory::handleBarcode)
+                .map(barcode -> handleCommand(barcodeInventory, barcode))
                 .forEach(System.out::println);
 
         System.out.println("Done.");
+    }
+
+    private static String handleCommand(BarcodeInventory barcodeInventory, String barcode) {
+        if (barcode.equals("total")) {
+            return "Total: $0.00";
+        }
+        return barcodeInventory.handleBarcode(barcode);
     }
 
     private static boolean isNotQuitCommand(String line) {
