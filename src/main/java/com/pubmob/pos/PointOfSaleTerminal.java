dd160b56bffc9d276cc86fe06d8e1bc40f8aeb45
Mark Rodden <mrodden@protocase.com>
2020-12-14 12:57:28 -0400

    mob next [ci-skip]

--- src/main/java/com/pubmob/pos/PointOfSaleTerminal.java
+++ src/main/java/com/pubmob/pos/PointOfSaleTerminal.java
@@ -1,18 +1,17 @@
 package com.pubmob.pos;
 
-import io.vavr.collection.HashMap;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class PointOfSaleTerminal {
+
     public static void main(String[] args) {
         System.out.println("Point of Sale Terminal.");
 
         new BufferedReader(new InputStreamReader(System.in)).lines()
                 .map(String::trim)
                 .takeWhile(PointOfSaleTerminal::isNotQuitCommand)
-                .map(PointOfSaleTerminal::handleBarcode)
+                .map(BarcodeInventory::handleBarcode)
                 .forEach(System.out::println);
 
         System.out.println("Done.");
@@ -22,17 +21,6 @@ public class PointOfSaleTerminal {
         return !"q".equals(line);
     }
 
-    private static String handleBarcode(final String barcode) {
-        final HashMap<String, Integer> barcodeToPrice = HashMap.of("12345", 550, "23456", 1015);
-
-        return barcodeToPrice.get(barcode)
-                .map(PointOfSaleTerminal::formatPrice)
-                .getOrElse("Error: barcode not found.");
-    }
-
-    private static String formatPrice(int cents) {
-        return String.format("$%.2f", cents / 100d);
-    }
 }
 
 
