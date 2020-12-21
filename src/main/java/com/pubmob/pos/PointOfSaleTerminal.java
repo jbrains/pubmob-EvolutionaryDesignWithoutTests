b04c2b07aea41247938da64db84fdfceaaf149f6
Peter Gfader <peter@beyond-agility.com>
2020-12-21 19:00:23 +0100

    r moved concern of Inventory from ShoppingCart

--- src/main/java/com/pubmob/pos/PointOfSaleTerminal.java
+++ src/main/java/com/pubmob/pos/PointOfSaleTerminal.java
@@ -4,32 +4,44 @@ import java.util.Map;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Optional;
 
 public class PointOfSaleTerminal {
     public static void main(String[] args) {
         System.out.println("Point of Sale Terminal.");
 
+        Map<String, Integer> inventory = Map.of("12345", 550, "23456", 1015);
         ShoppingCart shoppingCart = new ShoppingCart(
-                Map.of("12345", 550, "23456", 1015));
+        );
 
         new BufferedReader(new InputStreamReader(System.in)).lines()
                 .map(String::trim)
                 .takeWhile(PointOfSaleTerminal::isNotQuitCommand)
-                .map(barcode -> handleCommand(shoppingCart, barcode))
+                .map(barcode -> handleCommand(shoppingCart, barcode, inventory))
                 .forEach(System.out::println);
 
         System.out.println("Done.");
     }
 
-    private static String handleCommand(ShoppingCart shoppingCart, String command) {
+    private static String handleCommand(ShoppingCart shoppingCart, String command, Map<String, Integer> inventory) {
         if (command.equals("total")) {
             return "Total: " +
                     shoppingCart.getTotal();
         }
-        return shoppingCart.handleBarcode(command);
+        return handleBarcode(shoppingCart, command, inventory);
     }
 
     private static boolean isNotQuitCommand(String command) {
         return !"q".equals(command);
+    }
+
+    public static String handleBarcode(ShoppingCart shoppingCart, String barcode, Map<String, Integer> inventory) {
+        Optional<Integer> opPrice = Optional.ofNullable(inventory.get(barcode));
+
+        opPrice.ifPresent(price -> shoppingCart.addToCart(price));
+
+        return opPrice
+                .map(ShoppingCart::formatPrice)
+                .orElse("Barcode not found: " + barcode + ".");
     }
 }
--- src/main/java/com/pubmob/pos/ShoppingCart.java
+++ src/main/java/com/pubmob/pos/ShoppingCart.java
@@ -3,19 +3,15 @@ package com.pubmob.pos;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 public class ShoppingCart {
-    private final Map<String, Integer> inventory;
     private final List<Integer> prices;
 
-    public ShoppingCart(Map<String, Integer> inventory) {
-        this.inventory = inventory;
+    public ShoppingCart() {
         this.prices = new ArrayList<>();
     }
 
-    private static String formatPrice(int cents) {
+    static String formatPrice(int cents) {
         return String.format("$%.2f", cents / 100d);
     }
 
@@ -25,17 +21,7 @@ public class ShoppingCart {
         return formatPrice;
     }
 
-    public String handleBarcode(String barcode) {
-        Optional<Integer> opPrice = Optional.ofNullable(inventory.get(barcode));
-
-        opPrice.ifPresent(price -> addToCart(price));
-
-        return opPrice
-                .map(ShoppingCart::formatPrice)
-                .orElse("Barcode not found: " + barcode + ".");
-    }
-
-    private boolean addToCart(Integer price) {
+    boolean addToCart(Integer price) {
         return prices.add(price);
     }
 }
