d5d555ae5eb12f5fc1436423216c1b90459c6dea
Peter Gfader <peter@beyond-agility.com>
2020-12-21 19:06:42 +0100

    d added SMELL comment

--- src/main/java/com/pubmob/pos/ShoppingCart.java
+++ src/main/java/com/pubmob/pos/ShoppingCart.java
@@ -11,6 +11,7 @@ public class ShoppingCart {
         this.prices = new ArrayList<>();
     }
 
+    // SMELL: Wrong place
     static String formatPrice(int cents) {
         return String.format("$%.2f", cents / 100d);
     }
