2888974312a0bfa3cfab0e42113c18862edf5f28
J. B. Rainsberger <me@jbrains.ca>
2020-12-29 18:42:46 -0400

    Configure 'mob' to keep manual commits by default on 'mob done'.

--- script/configure-mob.sh
+++ script/configure-mob.sh
@@ -2,3 +2,5 @@
 export MOB_WIP_COMMIT_MESSAGE="[mob] autocommit WIP"
 export MOB_NEXT_STAY=true
 export MOB_WIP_BRANCH_QUALIFIER_SEPARATOR=/
+# Let's keep all our nice manual commits.
+export MOB_DONE_SQUASH=false
