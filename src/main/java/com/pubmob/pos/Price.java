d31f34a09cf8eb8acef3f959f301bc1245684414
J. B. Rainsberger <me@jbrains.ca>
2021-01-04 13:04:16 -0400

    We now report that GST applies to a product if it applies.

--- src/main/java/com/pubmob/pos/Price.java
+++ src/main/java/com/pubmob/pos/Price.java
@@ -1,14 +1,22 @@
 package com.pubmob.pos;
 
 public class Price {
+
     private final int cents;
+    private final boolean gstApplies;
+
+    public Price(final int cents, final boolean gstApplies) {
+        this.cents = cents;
+        this.gstApplies = gstApplies;
+    }
 
     public Price(int cents) {
-        this.cents = cents;
+        this(cents, false);
     }
 
     public String formatPrice() {
-        return String.format("$%.2f", cents / 100d);
+        final String gstApplied = gstApplies ? " G" : "";
+        return String.format("$%.2f%s", cents / 100d, gstApplied);
     }
 
     public int inCents() {
