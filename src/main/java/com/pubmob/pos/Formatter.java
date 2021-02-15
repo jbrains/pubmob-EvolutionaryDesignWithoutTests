a5304040ab7e2a9bec431922590cb9e13f344fe6
J. B. Rainsberger <me@jbrains.ca>
2021-02-15 13:26:31 -0400

    Formatting a string now accepts any number of arguments.

--- src/main/java/com/pubmob/pos/Formatter.java
+++ src/main/java/com/pubmob/pos/Formatter.java
@@ -1,7 +1,7 @@
 package com.pubmob.pos;
 
 public class Formatter {
-    public String formatString(final String template, final String arg1) {
-        return String.format(template, arg1);
+    public String formatString(final String template, final String... args) {
+        return String.format(template, args);
     }
 }
